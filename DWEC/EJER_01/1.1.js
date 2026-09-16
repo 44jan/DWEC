308e0a00c2584dd0bf860964def93d255146ac73
Jan Martos <ixd46363@educastur.es>
2026-09-16 10:51:53 +0200

    Add files via upload

---
+++ DWEC/EJER_01/1.1.js
@@ -0,0 +1,12 @@
+const nombre ="Jan"
+
+let edad = 20
+
+let tieneMascota = true
+// reasignar valores
+
+edad = 22
+tieneMascota = false
+
+console.log(nombre, edad, tieneMascota)
+console.log(`${nombre} tiene ${edad} años y no tiene mascota`  )
